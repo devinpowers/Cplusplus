eb203c90dbab517a2c82faffeaa2ec0827a54ecf
Devin Powers <powers88@msu.edu>
2020-12-01 15:31:32 -0500

    c++ stuff

---
+++ pyramid.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()  // main funciton "container that will get executed "
+
+{
+    cout << "   /|" << endl; // basic line of code in out program
+    cout << "  / |" << endl;
+    cout << " /  |" << endl;
+    cout << "/___|" << endl;
+
+    return 0;
+}
+
+
+
