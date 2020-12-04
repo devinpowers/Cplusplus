9455e0ca6e81cdde4700955fd80b82b61b54493b
Devin Powers <powers88@msu.edu>
2020-12-04 15:30:27 -0500

    c++

--- functions.cpp
+++ functions.cpp
@@ -1,11 +1,44 @@
-#include <iostream>
-using namespace std;
 
-int main() {
-  int i = 0;
-  while (i < 5) {
-    cout << i << "\n";
-    i++;
-  }
-  return 0;
+
+#include <iostream>
+#include <cmath>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+double power(double base, int exponent)  //definition and Decleration
+
+{
+    double result = 1;
+
+    for(int i = 0; i < exponent; i++)
+    {
+        result = result * base;
+    }
+    return result;
 }
+
+void print_pow(double base, int exponent)
+{
+  double myPower = power(base,exponent);
+  cout << base << " raised to the " << exponent << " power is " << myPower << ".\n";
+
+}
+
+int main()
+{
+  double base;
+  int exponent;
+
+  cout << "What is the base?:  ";
+  cin >> base;
+  cout << "What is the Exponent?: ";
+  cin >> exponent;
+  print_pow(base, exponent);
+
+  print_pow(100,4);
+  print_pow(4,2);
+}
+
