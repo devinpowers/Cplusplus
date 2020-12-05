35b173abadd1f6cfae63aa039a01100a9a73cd24
Devin Powers <powers88@msu.edu>
2020-12-05 10:33:24 -0500

    c++

--- more_c++.cpp
+++ more_c++.cpp
@@ -1,1 +1,17 @@
-//
+
+
+#include <iostream>
+using namespace std;
+ 
+// Global variable declaration:
+int g = 20;
+ 
+int main () {
+   // Local variable declaration:
+   int g = 10;
+ 
+   cout << g << endl;
+
+}
+
+
