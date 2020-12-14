1e07001339f07a308dfd817ae9893fd9bb844f6e
Devin Powers <powers88@msu.edu>
2020-12-14 13:27:21 -0500

    c++

--- stream/functions.cpp
+++ stream/functions.cpp
@@ -29,3 +29,67 @@ int main()
 
 
 
+Nested Vectors
+
+
+
+    #include <iostream>
+    #include <vector>
+
+    using namespace std;
+
+
+    int main()
+    {   
+        vector <vector<int>> grades =
+        {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+            
+        };
+
+
+        for(int r = 0; r < 3; r++)
+        {
+            for( int c =0; c <3; c++)
+            {
+                cout << grades[r][c] << "\t";
+            }
+            cout << "\n";
+        }
+
+        
+        
+        return 0;
+    }
+
+
+Multidimensional Array
+
+print with nested for loop
+
+
+    #include <iostream>
+
+
+    using namespace std;
+
+
+    int main()
+    {
+        int grades[][3] = {{1, 2, 3},{4, 5, 6}, {7, 8, 9}};
+
+        for(int r = 0; r < 3; r++)
+        {
+            for( int c =0; c <3; c++)
+            {
+                cout << grades[r][c] << "\t";
+            }
+            cout << "\n";
+        }
+
+        
+        
+        return 0;
+    }
