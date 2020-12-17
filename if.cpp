a55e163324660153ddf4f7bac5c53584e9032f0e
Devin Powers <powers88@msu.edu>
2020-12-17 09:35:30 -0500

    practicing c++

---
+++ if.cpp
@@ -0,0 +1,19 @@
+
+ #include <iostream>
+
+// if practice
+int main() {
+
+    int x = 12;
+
+    if (x < 10){
+
+        std::cout << "X is greater than 10" << std::endl;
+    }
+    else{
+
+        std::cout << " x is greater than 10" << std::endl;
+    }
+
+    return 0;
+}
--- practice/video.cpp
+++ practice/video.cpp
@@ -1,13 +1,16 @@
 
  #include <iostream>
-using namespace std;
+
  
 int main() {
 
-    string cars[4] = {"Volvo", "BMW", "Ford", "Mazda"};
-    cars[0] = "Opel";
-    cout << cars[0];
+    std::string cars[4] = {"Volvo", "BMW", "Ford", "Mazda"};
+    cars[0] = "Opel"; //change string 
+    std::cout << cars[0] << std::endl;
 
+    for ( int i = 0 ; i < cars[i]; i++ ){
+      std::cout << i << std::endl;
+    }
 
   return 0;
 }
