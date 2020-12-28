93ad1c56ebf68a5aa6db7581d1f1394ed907c6b0
Devin Powers <powers88@msu.edu>
2020-12-28 10:31:12 -0500

    working on c++

---
+++ Weeks/Week4/char.cpp
@@ -0,0 +1,19 @@
+
+#include<iostream>
+using std::cout; using std::cin; using std::endl;
+
+#include<string>
+using std::string;
+
+
+int main()
+{
+	string name = "Devin Powers" ;
+
+
+
+	cout << name[0] << endl;
+
+	cout << "PLease Print: " << endl;
+
+}
