bf20f00d6821f19a1e3c6cc3facd207a6c675173
Devin Powers <powers88@msu.edu>
2020-12-30 08:30:07 -0500

    learning c++

---
+++ Weeks/Week1/pac.cpp
@@ -0,0 +1,26 @@
+
+
+#include<iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+
+#include <string>
+using std::string;
+
+int main(){
+
+    string name = "Devin";
+    int age = 23;
+
+
+    cout << "Hello " << name << "," << endl
+           << "My name is Devin Powers " << endl
+    << "I am practicing shit right here " << endl
+           << 65 - age << "years to retirement" 
+         << endl;   
+
+    return 0;
+}
+
+
