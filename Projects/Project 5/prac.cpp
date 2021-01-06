a281ae5f325467c00930bc6789ff0d3f84009fc0
Devin Powers <powers88@msu.edu>
2021-01-06 07:50:28 -0500

    working on project 5 in c++

--- Projects/Project 5/prac.cpp
+++ Projects/Project 5/prac.cpp
@@ -4,44 +4,148 @@ using std::cout; using std::cin; using std::endl;
 using std::string;
 
 
-void print_pairs(string pairs)                  
+
+const string alphabet = "abcdefghijklmnoprstuvwxyz"; //without 'q'
+
+
+string clean_string (string s)  // Function for "Cleaning the String!!"
 {
-    for (auto letter : pairs)
+    string clean_string = "";
+  
+    for ( int i = 0; i < s.length(); i++)
     {
-        cout << "1st letter: " << letter << endl;
-    
+        if (s[i] != ' ')
+        {          
+            clean_string += tolower(s[i]); // lowers and cleans string ifthere is a space!
+        }
     }
-    cout << "End: " << endl;
+    return clean_string;
+
+
 }
+string create_encoding(string key){
+
+    string keyword = ""; // string that were returning 
+
+
+    for (auto character : key) {
+
+        if ( keyword.find(character) == -1)
+        {
+            keyword += character;
+        }
+    }
+
+    // need to add the rest of the alphabet to the end!
+
+    for (auto character : alphabet){
+
+        if ( keyword.find(character) == -1 ){
+
+            keyword += character;
+        }
+    }
+    return keyword;
+
+}
+
+string encode_digraph(string dg, string block1, string block2){
+    string result;
+
+    /*
+        string dg is a pair of letters passed to our function hre
+        Example:  "he" returns "fy"
+
+    */
+
+    char first_letter = dg[0];
+    char second_letter = dg[1];
+
+        /*  Location information:
+        row = index/5 (integer division)
+        col = index %5
+        index = row * 5 + col */
+    
+
+    int first_location = alphabet.find(first_letter);
+    int first_row = first_location/5;
+    int first_col = first_location%5;
+
+    int second_location = alphabet.find(second_letter);
+    int second_row = second_location/5;
+    int second_col = second_location%5;
+
+    //get new indexes from blocks
+    int first_new_index = first_row * 5 + second_col;
+    int second_new_index = second_row * 5 + first_col;
+
+
+    result += block1[first_new_index];
+    result += block2[second_new_index];
+
+    return result;
+
+}
+
+string encode(string msg, string key1, string key2){
+
+
+    string block1;
+    string block2;
+    string encode_message;
+    // Lets clean the msg first
+
+    string msg_clean;
+
+    msg_clean = clean_string(msg);
+
+    // send key1 and key2 to create_encoding
+
+    block1 = create_encoding(key1);
+    block2 = create_encoding(key2);
+
+
+    if (msg_clean.length() % 2 != 0)
+        {
+             msg_clean += "x";
+         }
+
+
+    for (int i = 0; i < msg_clean.length(); i++)
+    {
+        // send to encode_digraph
+
+        encode_message += encode_digraph( msg_clean.substr(i,2), block1, block2) ;
+
+        i++; // makes sure we skip iteration so we dont repeat a value
+    }
+
+    return encode_message;
+
+
+}
+
 
 
 
 int main() {
 
-    string msg = "Hellomynameisdevinpowerss";
-
-    cout << "Msg length: " << msg.length() << endl;
-
-    //if msg is odd add a "x" to the end
-
-    if (msg.length() % 2 != 0)
-    {
-        msg += "x";
-    }
-
-    // loop through msg and send pairs to function
-
-    for ( int i = 0; i < msg.length(); i++)
-        {
-            // send pairs to print_pairs function above
-
-            print_pairs(msg.substr(i,i+1));
-
-            i++;
-            i++;
-
-       
-        }
-
+    string message;
+    string keyword1;
+    string keyword2;
+    // gotta put getline() to grab the full line of the message to include spaces that might occur
+    cout << "Please Input a message to encode: ";
+    cin >> message;
+    cout << "Please input a keyword: " ;
     
+    cin >> keyword1;
+    cout << endl;
+    cout << "Please input another keyword: ";
+    cin >> keyword2;
+
+
+
+    cout << "our encoded squence for the word " << message << " is: " << encode(message, keyword1, keyword2) <<endl;
+
+ 
 }
