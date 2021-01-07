08718a34245b129a123ca14cb122dfddac7f7be2
Devin Powers <powers88@msu.edu>
2021-01-07 08:20:00 -0500

    try except in c++

--- Weeks/Week6/try.cpp
+++ Weeks/Week6/try.cpp
@@ -1,44 +1,19 @@
 
+// bad_typeid example
+#include <iostream>       // std::cout
+#include <typeinfo>       // operator typeid, std::bad_typeid
 
-#include<iostream>
-using std::cout; using std::cin; using std::endl;
-#include<string>
-using std::string;
-#include<stdexcept>
-using std::out_of_range; using std::invalid_argument;
-using std::runtime_error;
+class Polymorphic {virtual void Member(){}};
 
-int main (){
-  string my_str;
-  size_t indx;
-  long char_long;
-  char C;
-  
-  cin >> my_str;
-  cin >> indx;
-  
-  try {
-    C = my_str.at(indx);                   // thows out_of_range
-    char_long = stol( string(1,C) );         // throws invalid_argument
-    if (char_long == 0)
-      throw runtime_error("division by zero");
-    cout << my_str.size() / char_long << endl;
-  } catch (out_of_range& e) {
-    cout << "In the out of range catcher" << endl;
-    cout << e.what() << endl;
-  } catch (invalid_argument& e) {
-    cout << "in the invalid_arg catcher" << endl;
-    cout << e.what() << endl;
+int main () {
+  try
+  {
+    Polymorphic * pb = 0;
+	std::cout << typeid(*pb).name();
   }
-  /*
-  catch(...){
-    cout << "something bad happened" << endl;
+  catch (std::bad_typeid& bt)
+  {
+    std::cerr << "bad_typeid caught: " << bt.what() << '\n';
   }
-  */
-    
-  // past the try-catch construct
-  cout << "finishing up" << endl;
+  return 0;
 }
-
-
-
