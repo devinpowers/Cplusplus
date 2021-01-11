9d8b007c1c51da01c6661ea7d17a9c808fad8dc6
Devin Powers <powers88@msu.edu>
2021-01-11 08:27:09 -0500

    c++ learning

---
+++ Weeks/Week8/prac.cpp
@@ -0,0 +1,15 @@
+
+#include<iostream>
+using std::cout; using std::cin; using std::endl;
+#include<vector>
+using std::vector; using std::iterator;
+#include<string>
+using std::string;
+
+
+int main()
+{
+    cout << "Yes" << endl;
+
+    return 0;
+}
