4f02ae178b0509191146f6a22733b6be2b59694e
Devin Powers <powers88@msu.edu>
2021-01-27 13:14:17 -0500

    C++ Classes

---
+++ Weeks/Week9/prac.cpp
@@ -0,0 +1,23 @@
+
+#include<iostream>
+using std::cout; using std::endl;
+#include <map> 
+using std::map;
+#include<string>
+using std::string;
+
+
+struct Clock {
+
+    int minutes;
+    int hours;
+    string period;
+
+
+};
+
+
+int main(){
+
+    cout << "Yes " << endl;
+}
