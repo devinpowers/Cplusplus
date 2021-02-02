4663b58fd5656de05122d8f03cabcb1f217d7299
Devin Powers <powers88@msu.edu>
2021-02-02 16:09:13 -0500

    classes

---
+++ Weeks/Week9/more_oop.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using std::cout; using std::endl;
+#include<string>
+using std::string;
+
+
+
+// represents Blueprint
+class Employee{
+    // inside the class we need to put attributes and behaviors
+private:
+    string name;
+    string company;
+    int age;
+
+};
+
+
+int main()
+{
+    Employee employee1;
+
+    employee1.
+    
+
+}
