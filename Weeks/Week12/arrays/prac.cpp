d204b269c59648003740fd5603b03ff0d9dcfe21
Devin <powers88@msu.edu>
2021-06-30 07:38:31 -0400

    update arrays

--- Weeks/Week12/arrays/prac.cpp
+++ Weeks/Week12/arrays/prac.cpp
@@ -1,34 +1,68 @@
 
-
 #include <iostream>
-using std::endl;
-using std::cout;
-#include<algorithm>
-using std::copy; using std::sort; using std::transform;
-#include<iterator>
-using std::ostream_iterator; using std::begin; using std::end;
-#include<string>
+using namespace std;
 
-int main(){
-   int arr[] = {20, 30, 40, 50};
-   int size = 4;
+class Box {
+   public:
+      double getVolume(void) {
+         return length * breadth * height;
+      }
+      void setLength( double len ) {
+         length = len;
+      }
+      void setBreadth( double bre ) {
+         breadth = bre;
+      }
+      void setHeight( double hei ) {
+         height = hei;
+      }
+      
+      // Overload + operator to add two Box objects.
+      Box operator+(const Box& b) {
+         Box box;
+         box.length = this->length + b.length;
+         box.breadth = this->breadth + b.breadth;
+         box.height = this->height + b.height;
+         return box;
+      }
+      
+   private:
+      double length;      // Length of a box
+      double breadth;     // Breadth of a box
+      double height;      // Height of a box
+};
 
-  int *ptr_arr_front = arr;
-  int *ptr_arr_back = arr+size;			
+// Main function for the program
+int main() {
+   Box Box1;                // Declare Box1 of type Box
+   Box Box2;                // Declare Box2 of type Box
+   Box Box3;                // Declare Box3 of type Box
+   double volume = 0.0;     // Store the volume of a box here
+ 
+   // box 1 specification
+   Box1.setLength(6.0); 
+   Box1.setBreadth(7.0); 
+   Box1.setHeight(5.0);
+ 
+   // box 2 specification
+   Box2.setLength(12.0); 
+   Box2.setBreadth(13.0); 
+   Box2.setHeight(10.0);
+ 
+   // volume of box 1
+   volume = Box1.getVolume();
+   cout << "Volume of Box1 : " << volume <<endl;
+ 
+   // volume of box 2
+   volume = Box2.getVolume();
+   cout << "Volume of Box2 : " << volume <<endl;
 
-  cout << "The Front ptr: "<< ptr_arr_front << endl;
-  cout << "1 past End ptr: "<< ptr_arr_back << endl; 
+   // Add two object as follows:
+   Box3 = Box1 + Box2;
 
+   // volume of box 3
+   volume = Box3.getVolume();
+   cout << "Volume of Box3 : " << volume <<endl;
 
-  // no begin or end method, but an array pointer IS an iterator
-  sort(ptr_arr_front, ptr_arr_back);
-  copy(ptr_arr_front, ptr_arr_back, ostream_iterator<int>(cout, ", ") );
-  cout << endl;
-
-    // begin() and end() functions  (not methods) do work
-  // IF compiler knows size
-  transform(begin(arr), end(arr), ostream_iterator<int>(cout, ", "),
-       [](int x) {return x*2;}
-       );
-  cout << endl;
+   return 0;
 }
