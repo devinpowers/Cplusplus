cd099d8d757ccf080fd91891b3f03a1b90e55381
Devin <powers88@msu.edu>
2021-07-11 12:38:49 -0400

    working on Project 10

---
+++ Projects/Project 10/class-10.h
@@ -0,0 +1,21 @@
+#ifndef BISTACK_H
+#define BISTACK_H
+
+#include<iostream>
+using std::ostream;
+#include<string>
+using std::string; using std::to_string;
+#include<iterator>
+using std::ostream_iterator;
+#include<sstream>
+using std::ostringstream;
+#include<algorithm>
+using std::copy;
+using std::swap;
+#include<initializer_list>
+using std::initializer_list;
+
+
+
+#endif
+ 
