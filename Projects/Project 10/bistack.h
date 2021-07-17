13b987a04abae41d98ec0b665fdde6db17d972f7
Devin <powers88@msu.edu>
2021-07-17 14:40:57 -0400

    working thru project 10

--- Projects/Project 10/bistack.h
+++ Projects/Project 10/bistack.h
@@ -34,20 +34,20 @@ class BiStack{
         int max_size_ = 16;
         int top1_ = -1;
         int top2_ = size_;
-        void grow_and_copy();
+        void grow_and_copy(); // Private Method to double our array if needed!
 
     public:
 
     // Constructors
-    BiStack() = default;
-    BiStack( size_t initial_size, size_t max_size = 16);
-    BiStack(initializer_list<ElementType>, size_t max_size = 16);
+    BiStack() = default; // Default
+    BiStack( size_t initial_size, size_t max_size = 16);   // size_t will be a int like -> BiStack(4) 
+    BiStack(initializer_list<ElementType>, size_t max_size = 16); // initializer_list will be a literal list like -> BiStack stack1 = {2,5,4,3,2,4,9}
 
     // Copy Constructor
     BiStack(const BiStack &s);
     // Deconstructor
     ~BiStack();
-    // Overloaded =
+    // Overloaded = Operator
     BiStack& operator=(BiStack);
 
     // Methods
@@ -65,42 +65,44 @@ class BiStack{
     void push1(ElementType);
     void push2(ElementType);
 
-    bool empty1();
-    bool empty2();
+    bool empty1(); // Checks if Stack 1 is Empty
+    bool empty2(); // Checks if Stack 2 is Empty
 
-    friend ostream& operator<< <ElementType>(ostream&, const BiStack&);
+    friend ostream& operator<< <ElementType>(ostream&, const BiStack&); // Returns stream aka prints out!!
 };
 
 template<typename ElementType>
 BiStack<ElementType>::BiStack(size_t initial_size, size_t max_size)
 {
     // Size constructor that initializes the variables
-    size_ = initial_size;
-    max_size_ = max_size;
+    // BiStack stack2(8); <- Example using this constructor
+    size_ = initial_size; // Which would be size 8 in our example
+    max_size_ = max_size; // 16
     ary_ = new ElementType[size_]; // Creates new dynamically allocated array of size
-    top1_ = -1;
-    top2_ = size_;
-  
-}
+    top1_ = -1; // Indexing 
+    top2_ = size_; // Indexing 
+} 
 
+// List Constructor
 template<typename ElementType>
 BiStack<ElementType>::BiStack(initializer_list<ElementType> list_, size_t max_size)
 {
     // List Constructor Example -> {3,4,5,6,1,2,3,4}
-    size_ = list_size();
-    max_size_ =max_size;
-    top1_ = -1;
-    top2_ = size_;
+    size_ = list_.size(); // Size of the initializer_list that we past in
+    max_size_ = max_size; // which will be 16
+    top1_ = -1; // Indexes
+    top2_ = size_; // Indexes
     // Create new Array to hold our list items
     ary_ = new ElementType[size_]{};
-    size_ index = 0;
+    size_t index = 0;
 
+    // Copy list elements into the array
     for (auto e : list_)
     {
         ary_[index++] = e;
     }
     // Update the top of Stack 1
-    top1_ = index =1;
+    top1_ = index - 1;
 }
 
 // The Copy Constructor
@@ -119,6 +121,121 @@ BiStack<ElementType>::BiStack(const BiStack<ElementType> &s)
     copy(s.ary_, s.ary_+s.size_, ary_); // Copy Elements from array to new construtor
 }
 
+
+// Assignment Operator
+template<typename ElementType>
+BiStack<ElementType>& BiStack<ElementType>::operator=(BiStack<ElementType> stack)
+{
+    swap(this->size_, stack.size_);
+    swap(this->max_size_, stack.max_size_);
+    
+    swap(this->top1_, stack.top1_);
+    swap(this->top2_, stack.top2_);
+    
+    swap(this->ary_, stack.ary_);
+    
+    return *this;
+}
+
+
+// Destructor
+
+template <typename ElementType>
+BiStack<ElementType>::~BiStack(){
+    //Destructor
+    delete [] ary_;
+}
+
+// Copy and Grow
+template <typename ElementType>
+void BiStack<ElementType>::grow_and_copy()
+{
+    ElementType *new_array;
+
+    if (size_ == 0)
+    {
+        new_array = new ElementType[1]{};
+        size_ =1;
+        ary_ = new_array;
+    }
+
+    else
+    {
+        // We double the array in size without growing it past the max size
+        if (size_*2 <= max_size_ )
+        {
+            // We create a new Array
+            new_array = new ElementType[size_ * 2]{}; // New array constructor
+            int new_array_size = size_*2;
+
+            // Copy Elements from old array into the new array (tricky)
+            copy(ary_, ary_ + top1_+1, new_array); // Front part of array
+
+            int number_of_elements = size_ - top2_;
+            for (int i = i; i <= number_of_elements; i++)
+            {
+                // insert elements in back of the array
+                new_array[new_array_size - i] = ary_[size_ -i];
+            }
+            // Now we have to update Size and Tops in our New Array
+            int difference = size_ - top2_;
+            size_ *= 2;
+            top2_ = size_ - difference;
+
+            // Swap Arrays
+            std::swap(new_array, ary_);
+            // Prevent Memory Leake
+            delete [] new_array;
+        }
+        // Else the array is already too big and we have to throw an error
+        else
+        {
+            throw std::overflow_error("Stack is already past it's max!");
+        }
+    }
+
+}
+
+// OK now I have to just do push1 and push2 methods
+
+
+template <typename ElementType>
+void BiStack<ElementType>::push1(ElementType element)
+{
+    // If array is full, we can call the copy_and_grow method to try and grow the array
+    if (top1_ == top2_ - 1)
+    {
+        grow_and_copy();
+        ary_[++top1_] = element;
+    }
+    // Otherwise it's not full and we can add an element to the front
+    else
+    {
+        ary_[++top1_] = element;
+    }
+
+}
+
+
+template <typename ElementType>
+void BiStack<ElementType>::push2(ElementType element)
+{
+    // If array is full, we can call the copy_and_grow method to try and grow the array
+    if (top2_ == (top1_ + 1))
+    {
+        grow_and_copy();
+        // ADDD??
+    }
+    // Otherwise it's not full and we can add an element to the front
+    else
+    {
+        ary_[--top2_] = element;
+    }
+    
+}
+
+
+
 template <typename ElementType>
 ElementType BiStack<ElementType>::top1()
 {    // Similar to the Stack used in Week Examples
@@ -134,8 +251,7 @@ ElementType BiStack<ElementType>::top1()
 
 template <typename ElementType>
 ElementType BiStack<ElementType>::top2()
-{    // Free Cuba! (FREE CUBA)
-
+{   
     if (top2_ == size_ )
     {
         throw underflow_error("Top2, empty stack");
@@ -152,50 +268,56 @@ size_t const BiStack<ElementType>::max()
     return max_size_;
 }
 
+
 template <typename ElementType>
-size_t const BiStack<ElementType::size()
+size_t const BiStack<ElementType>::size()
 {
     // Return the number of elements in stacks
-    return top1_ + 1 + (size_ - top2_);
+    return top1_ +1 + (size_ - top2_);
 
 }
+
+
 template <typename ElementType>
-size_t const BiStack<ElementType::capacity()
+size_t const BiStack<ElementType>::capacity()
 {
     // Return the current size
     return size_;
 }
 
+
 template <typename ElementType>
-void BiStack<ElementType::pop1()
-{
+void BiStack<ElementType>::pop1()
+{   // "Pop" element from stack1
     if (top1_ < 0)
     {
         throw underflow_error("pop, empty stack");
     }
+    // Update top index to remove element
     --top1_;
 
 }
 
 template <typename ElementType>
-void BiStack<ElementType::pop2()
+void BiStack<ElementType>::pop2()
 {
-        if (top2_ == size_)
+    if (top2_ == size_)
     {
         throw underflow_error("pop, empty stack 2 ");
     }
+    // Update top2 index to remove element
     ++top2_;
 }
 
 template <typename ElementType>
-bool BiStack<ElementType::empty1()
+bool BiStack<ElementType>::empty1()
 {
     // Checks if there are elements in stack 1
     return top1_ == -1;
 }
 
 template <typename ElementType>
-bool BiStack<ElementType::empty2()
+bool BiStack<ElementType>::empty2()
 {
     // Checks if there are elements in stack 2
     return top2_ == size_;
@@ -209,6 +331,7 @@ ostream& operator<<(ostream &out, const BiStack<ElementType> &stack)
     ostringstream oss;
     string outstring;
 
+    // Lets print out the first stack1
     if (stack.top1_ != -1)
     {
         out << "Top1 index: " << stack.top1_ << endl;
@@ -222,12 +345,24 @@ ostream& operator<<(ostream &out, const BiStack<ElementType> &stack)
         out << "Top2 Index: " << stack.top2_ << endl;
     }
     else{
+        // Else it's empty
         out << "Top2 is Empty" << endl;
     }
 
     // Let's print out the Size and Capacity of the BiStack
-    out << "Size: " << size_.top1_+1 + (size_.size - size_.top2_) << endl;
+    out << "Size: " << stack.top1_+1 + (stack.size_ - stack.top2_) << endl;
+    // Print out Capacity of the BiStack
     out << "Capacity of BiStack: " << stack.size_ << endl;
+
+    // Let's now print the Array
+
+    copy(stack.ary_, (stack.ary_ + stack.size_), ostream_iterator<ElementType>(oss, ","));
+
+    outstring = oss.str(); // Convert oss to string
+    outstring = outstring.substr(0, outstring.size() - 1);
+    out << outstring;
+
+    return out;
 
 }
 
