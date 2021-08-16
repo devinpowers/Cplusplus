463875f1c8ef275ec67691ffbe4f86667c42f5cd
devinpowers <powers88@msu.edu>
2021-08-16 08:24:04 -0400

    update C++ page

--- algorithms/sorting algorithms/merge.cpp
+++ algorithms/sorting algorithms/merge.cpp
@@ -1,14 +1,11 @@
+
+
 #include <iostream>
-using namespace std;
-// example of merge sort in C/C++
-// merge function take two intervals
-// one from start to mid
-// second from mid+1, to end
-// and merge them in sorted order
+using std::cout; using std::endl;
 
 void printArray(int arr[], int size);
 
-void merge(int *Arr, int start, int mid, int end) {
+void merge(int Arr[], int start, int mid, int end) {
 	// create a temp array
 	int temp[end - start + 1];
 
@@ -47,35 +44,20 @@ void merge(int *Arr, int start, int mid, int end) {
   // Can I print the array?
 
   cout << "After Calling Merge: ";
-  printArray(Arr, 8);
+  printArray(Arr, 5);
 }
 
 // Arr is an array of integer type
 // start and end are the starting and ending index of current interval of Arr
 
-void mergeSort(int *Arr, int start, int end) {
+void MergeSort(int Arr[], int start, int end) {
 
 	if(start < end) {
-    int count = 0;
-    count += 1;
 		int mid = (start + end) / 2;
-    cout << "END: " << end << endl;
-		mergeSort(Arr, start, mid);
-    cout << "++++++++++ " << endl;
-    cout << "Start: " << start << endl;
-    cout << "Middle: " << mid << endl;
-    cout << "ENd: " << end << endl;
-    printArray(Arr, 7);
+		MergeSort(Arr, start, mid);
 
     // Mid once the mergeSort call is done with the left side will be equal to 0 
-		mergeSort(Arr, mid+1, end);
-
-    // cout << "Right Half of Array " << endl;
-
-   // cout << "Middle: " << mid << endl;
-    // cout << "Start: " << start << endl;
-    cout << "End: " << end << endl;
-    
+		MergeSort(Arr, mid+1, end);
 		merge(Arr, start, mid, end);
 
 	}
@@ -95,7 +77,7 @@ int main()
 {
 
   // array to pass
-  int arr[] = {14,7,3,12,9,11,6,2};
+  int arr[] = {9,7,3,6,2};
 
   
   int size = sizeof(arr) / sizeof(arr[0]);
@@ -104,16 +86,17 @@ int main()
 
   cout << "Array before being sorted: ";
   printArray(arr, size);
-  mergeSort(arr, start, size -1);  
+  cout << "OK";
+  cout << endl;
+  MergeSort(arr, start, size -1);  
 
   cout << endl;
   cout << "Array AFTER being sorted: ";
   printArray(arr, size);
 
-  int a,b;
 
-  a = 7;
-  b = 0;
+  cout << "___________________________" << endl;
 
-  cout << "a/b: " << (a + b)/2 << endl;
+  printArray(arr,size); 
 }
+
