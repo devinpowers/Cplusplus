e9662896c10d3a11a035019914c6e3782099be4f
devinpowers <powers88@msu.edu>
2021-08-30 10:50:19 -0400

    update

--- data_structures/linked_lists/insert_node.cpp
+++ data_structures/linked_lists/insert_node.cpp
@@ -15,7 +15,7 @@ Node* head; // Global Variable, can be accessed anywhere
 void Insert(int x)
 {
     Node* temp = new Node(); // Create New Node to Insert
-    temp->data = x;
+    temp->data = x;  // De-Reference to Modify the value at this particular address
     temp->next = head;
     head = temp;
 }
@@ -24,14 +24,14 @@ void print()
 {
 
     // Print Linked Lists
-	Node* temp = head; // address of the head node
-    // Were adding on to this head node the addresses of the other nodes 
+	Node* temp = head; // address of the head node pointing to the first node in the linked list
 
     cout << "List is: ";
 
 	while(temp!=NULL){
-        
+        // Iterate through
 		cout<<temp->data<<"->";
+
 		temp=temp->next;
 	}
 	cout<< endl;
@@ -40,7 +40,6 @@ void print()
 
 int main()
 {
-
     head = NULL; // Empty Linked List
 
     cout << "How Many numbers? \n";
@@ -52,12 +51,14 @@ int main()
         cout << "Enter the number \n";
         cin >> x;
          Insert(x);
-         print();
+         print(); // Print out Current linked List
     }
 
-    cout << "END" << endl;
+    cout << "W" << endl;
 }
 
+//The Final thing we need to do, it to write the address of this 
+//newly created node in A.
 
-
+//OUr Linked list is intact now, we have one node!
 
