1a50f96ed997282d7fed7d55591b76976eb1a3e8
devinpowers <powers88@msu.edu>
2022-01-17 17:06:40 -0500

    update

---
+++ Computer Systems/L4/cp2.c
@@ -0,0 +1,10 @@
+#define ARRAY_SIZE 100
+
+int main() {
+    int a[ARRAY_SIZE];
+    int i;
+    for (i = 0; i < ARRAY_SIZE; i++) 
+	a[i] = 0;
+    return 0;
+} 
+
---
+++ Computer Systems/L4/cpp1.cpp
@@ -0,0 +1,8 @@
+int main() {
+    int a[100];
+    int i;
+
+    for (i = 0; i < 10; i++) 
+	a[i] = 0;
+    return 0;
+}
